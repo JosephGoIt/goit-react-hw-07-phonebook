c8137ad1f5c10c2c7d17d89df641ca6b40890099
Joseph Camacho <spongkj@gmail.com>
2024-07-19 22:08:48 +0800

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Section from './Section';
+import { store } from '../redux/store';
+import { fetchContacts } from '../redux/slices/contactsSlice';
+
 export const App = () => {
+  useEffect(() => {
+    store.dispatch(fetchContacts());
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Provider store={store}>
+      <div>
+        <Section title="Phonebook">
+          <ContactForm />
+        </Section>
+        <Section title="Contacts">
+          <Filter />
+          <ContactList />
+        </Section>
+      </div>
+    </Provider>
   );
 };
+
---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import contactsReducer from './slices/contactsSlice';
+import filterReducer from './slices/filterSlice';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
